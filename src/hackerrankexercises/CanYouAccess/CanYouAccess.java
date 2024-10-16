3016122ffd043cbe87f8c3ca200bc82baaafc345
Berk <mberktuncer@gmail.com>
2024-10-16 15:39:58 +0300

    CanYouAccess added.

---
+++ src/hackerrankexercises/CanYouAccess/CanYouAccess.java
@@ -0,0 +1,30 @@
+package hackerrankexercises.CanYouAccess;
+
+import java.util.Scanner;
+
+public class CanYouAccess {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        Object o;// Must be used to hold the reference of the instance of the class Solution.Inner.Private
+
+        //Write your code here
+        Inner inner = new Inner();
+        Inner.Private private1 = inner.new Private();
+        System.out.println(num + " is " + private1.powerof2(num));
+        o = private1;
+
+
+        System.out.println("An instance of class: " + o.getClass().getCanonicalName() + " has been created");
+    }
+    static class Inner{
+        private class Private{
+            private String powerof2(int num){
+                return ((num&num-1)==0)?"power of 2":"not a power of 2";
+            }
+        }
+    }
+
+}
