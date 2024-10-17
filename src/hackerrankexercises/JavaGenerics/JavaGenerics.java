81ba677c9f1cb004f375907dc927fddf14f50c0d
Berk <mberktuncer@gmail.com>
2024-10-17 16:57:16 +0300

    Java Generics added.

---
+++ src/hackerrankexercises/JavaGenerics/JavaGenerics.java
@@ -0,0 +1,20 @@
+package hackerrankexercises.JavaGenerics;
+
+public class JavaGenerics {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Integer[] a = {1, 2, 3};
+        String[] b = {"Hello", "World"};
+
+        printArray(a);
+        printArray(b);
+    }
+
+    public static <T> void printArray(T[] array){
+        for(T i : array){
+            System.out.println(i);
+        }
+    }
+
+}
