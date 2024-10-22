97dbafab189863f586aace7d38be5c698f39f897
Berk <mberktuncer@gmail.com>
2024-10-22 14:47:02 +0300

    Java Exception Handling Try Catch added.

---
+++ src/JavaExceptionHandlingTryCatch/JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,23 @@
+package JavaExceptionHandlingTryCatch;
+
+import java.util.Scanner;
+
+public class JavaExceptionHandlingTryCatch {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        try{
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getClass().getName() + ": / by zero");
+        }
+        catch(Exception e){
+            System.out.println(e.getClass().getName());
+        }
+    }
+
+}
